f15bee5fcfbedba457253bc6ca9758d49f7096f3
Max S <mvsereb3@mts.ru>
2013-07-09 13:32:41 +0400

    Sort java-file to packages

---
+++ src/ru/mts/admsk/mvsereb3/PackageTest.java
@@ -0,0 +1,11 @@
+package ru.mts.admsk.mvsereb3;
+
+//import ru.mts.admsk.mvsereb3;
+
+public class PackageTest {
+	public static void main(String[] args) {
+		System.out.println("Package Hello");
+		ClassPathTest cp = new ClassPathTest();
+		System.out.println(cp.name);
+	}
+}
