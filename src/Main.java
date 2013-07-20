a91883e550c4796ef377490487c34ad800c3a47f
Max S <maxmotos@live.ru>
2013-07-20 22:03:48 +0400

    Added Main class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: max
+ * Date: 20.07.13
+ * Time: 21:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
