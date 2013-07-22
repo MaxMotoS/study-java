b2fe00f22a18182e829c53bcbf7b22438eec7fa3
Max S <mvsereb3@mts.ru>
2013-07-22 16:18:41 +0400

    new file:   src/max/ttt/TicTacToe.java

---
+++ src/SimpleHttpRequest.java
@@ -0,0 +1,41 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mvsereb3
+ * Date: 22.07.13
+ * Time: 12:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleHttpRequest {
+    public static void main(String[] args) throws IOException {
+        URL serverAddress = null;
+        HttpURLConnection connection = null;
+        DataInputStream in = null;
+        String data = null;
+
+        serverAddress = new URL("http://" + args[0]);
+
+        connection = (HttpURLConnection)serverAddress.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+        connection.setReadTimeout(10000);
+
+        connection.connect();
+
+        in = new DataInputStream(connection.getInputStream());
+
+        while ((data = in.readUTF()) != null) {
+            System.out.println(data);
+        }
+
+        connection = null;
+        serverAddress = null;
+        in = null;
+    }
+}
---
+++ src/max/ttt/TicTacToe.java
@@ -0,0 +1,55 @@
+package max.ttt;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mvsereb3
+ * Date: 22.07.13
+ * Time: 15:46
+ * To change this template use File | Settings | File Templates.
+ */
+enum Cell {X, O, _}
+
+public class TicTacToe {
+    private final static int SIZE_FIELD = 3;
+    private final static Cell DEFAULT_CELL = _;
+    private final static int FIRST_CHECK = 4;
+
+    private int countMoves = 0;
+    private Cell[][] field = null;
+
+    private Cell[][] newField() {
+        Cell[][] temp = new Cell[SIZE_FIELD][];
+        for (int i = 0; i < SIZE_FIELD; i++) {
+            temp[i] = newLine();
+        }
+        return temp;
+    }
+
+    private Cell[] newLine() {
+        Cell[] temp = new Cell[SIZE_FIELD];
+        for (int i = 0; i < SIZE_FIELD; i++) {
+            temp[i] = DEFAULT_CELL;
+        }
+        return temp;
+    }
+
+    public void start() {
+        field = newField();
+        countMoves = 0;
+    }
+
+    public void makeMove(int x, int y, Cell cell) {
+        if (x >= SIZE_FIELD || y >= SIZE_FIELD) {
+            // выброс ошибки
+        } else {
+            field[x][y] = cell;
+            if (++countMoves > FIRST_CHECK) {
+                check();
+            }
+        }
+    }
+
+    private void check() {
+
+    }
+}
