9d6e80ff9f6f389422756e0ccb7d6387fa0b2a20
Max S <maxmotos@mts.ru>
2013-07-30 16:44:47 +0400

    OK

---
+++ src/OK.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mvsereb3
+ * Date: 30.07.13
+ * Time: 16:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class OK {
+}
